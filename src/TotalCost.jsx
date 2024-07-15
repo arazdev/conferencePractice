957d2679266e20da4f0245f8f344fd936ca210ef
arazdev <hello@example.com>
2024-07-15 15:45:35 -0500

    Practice Done

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import "./TotalCost.css";
 
-const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
+const TotalCost = ({ totalCosts, handleClick, ItemsDisplay }) => {
+  const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
 
   return (
     <div className="pricing-app">
@@ -11,14 +11,12 @@ const TotalCost = ({ totalCosts, ItemsDisplay }) => {
           <p className="preheading"><h3>Total cost for the event</h3></p>
         </div>
         <div>
-          <h2 id="pre_fee_cost_display" className="price">
-           
-          </h2>
-         
-            <div>
-             
-            </div>
+          <h2 id="pre_fee_cost_display" className="price">${total_amount}</h2>
+          <div className="render_items">
+            <ItemsDisplay />
+          </div>
         </div>
+        <button onClick={handleClick} className="toggle-button">Toggle Items</button>
       </div>
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avSlice from './avSlice';
+import mealsSlice from './mealsSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avSlice,
+    meals: mealsSlice
   },
 });
